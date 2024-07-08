ae179f267d5d6e13e3a44f529cb7da6b7adb9717
sashhw <sash.walk@gmail.com>
2024-07-08 12:29:06 -0500

    Initial Commit

---
+++ FetchDesserts/FetchDessertsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchDessertsApp.swift
+//  FetchDesserts
+//
+//  Created by Sasha on 7/8/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchDessertsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
